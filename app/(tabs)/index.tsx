b4ac68896ff6b71143b9956e169ef5cfbef54f02
najongjine <najongjin3@hotmail.com>
2026-01-27 09:36:19 +0900

    .

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -172,7 +172,6 @@ export default function HomeScreen() {
       >
         {/* 1. Header Section (Preserving Lottie) */}
         <View style={styles.headerContainer}>
-          <Text>{token}</Text>
           <LottieView
             source={require("../../assets/lottie/Bunny_Hop.json")}
             style={styles.headerLottie}
